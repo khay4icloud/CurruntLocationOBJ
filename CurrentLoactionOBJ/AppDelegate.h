c3a33628e6b793ea9b376f1104f401b3d913fb3e
Sri Kalyan Ganja <Kal_Yan@sris-macbook-pro.local>
2017-04-18 12:48:16 -0500

    Initial Commit

---
+++ CurrentLoactionOBJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CurrentLoactionOBJ
+//
+//  Created by Sri Kalyan Ganja on 4/18/17.
+//  Copyright © 2017 KLYN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CurrentLoactionOBJ/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CurrentLoactionOBJ
+//
+//  Created by Sri Kalyan Ganja on 4/18/17.
+//  Copyright © 2017 KLYN. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
